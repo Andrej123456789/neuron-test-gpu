54095f16ccf3ae266f746cfe34e9c4c5d9faeea6
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2025-01-06 21:56:43 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# neuron-test-gpu
+
+GPU version of simple neural network test I made in [Python](https://github.com/Andrej123456789/neuron-test)
+
+Neural network implemented from Tech Altar's video.
+
+## Compile
+1) Install CUDA toolkit
+2) Compile a Visual Studio solution
---
+++ kernel.cu
@@ -0,0 +1,47 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void vectorAdd(int* a, int* b, int* c)
+{
+    int i = threadIdx.x;
+    c[i] = a[i] + b[i];
+    
+    return;
+}
+
+int main()
+{
+    int a[] = { 1, 2, 3 };
+    int b[] = { 4, 5, 6 };
+    int c[sizeof(a) / sizeof(int)] = { 0 };
+
+    int* cudaA = 0;
+    int* cudaB = 0;
+    int* cudaC = 0;
+
+    cudaMalloc(&cudaA, sizeof(a));
+    cudaMalloc(&cudaB, sizeof(b));
+    cudaMalloc(&cudaC, sizeof(c));
+
+    cudaMemcpy(cudaA, a, sizeof(a), cudaMemcpyHostToDevice);
+    cudaMemcpy(cudaB, b, sizeof(b), cudaMemcpyHostToDevice);
+    cudaMemcpy(cudaC, c, sizeof(c), cudaMemcpyHostToDevice);
+
+    vectorAdd <<< 1, sizeof(a) / sizeof(int) >>> (cudaA, cudaB, cudaC);
+    cudaMemcpy(c, cudaC, sizeof(c), cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < sizeof(c) / sizeof(int); i++)
+    {
+        c[i] = a[i] + b[i];
+    }
+
+    for (int i = 0; i < sizeof(c) / sizeof(int); i++)
+    {
+        printf("%d: %d\n", i, c[i]);
+    }
+
+    return 0;
+}
